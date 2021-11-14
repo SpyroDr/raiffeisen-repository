fc0b5d1ad6ac601d31e00e4d67078cef63724abc
SpyroDr <yury.sharypov@yandex.ru>
2021-11-14 19:58:02 +0700

    добавил build.Gradle

---
+++ java-examples/src/test/java/LoginTest.java
@@ -0,0 +1,40 @@
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class LoginTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+
+    }
+
+
+    @Test
+    public void loginTest() {
+        driver.navigate().to("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        wait.until(titleIs("My Store"));
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
