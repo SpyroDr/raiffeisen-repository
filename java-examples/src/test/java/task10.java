72ac42bcbae0896a1f2d72679a033c4dd7ba3d4e
SpyroDr <yury.sharypov@yandex.ru>
2021-12-05 03:21:37 +0700

    Добавил ассерты на цвет, числовое сравнение

--- java-examples/src/test/java/task10.java
+++ java-examples/src/test/java/task10.java
@@ -1,17 +1,30 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.hamcrest.MatcherAssert;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
+
+
+import java.awt.*;
+import java.util.StringTokenizer;
 import java.util.concurrent.TimeUnit;
 
+import static org.hamcrest.CoreMatchers.endsWith;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+
 public class task10 {
-    private WebDriver driver;
+
+    WebDriver driver;
+
+
 
     @Before
     public void start() {
@@ -31,6 +44,7 @@ public class task10 {
         String [] listItemPage;
         String result;
 
+
         WebElement item = driver.findElement(By.xpath(".//div[@id='box-campaigns']//li[1]"));
         listMainPage = fillList(item);
         listMainPage[0] = item.findElement(By.cssSelector("div.name")).getText();
@@ -43,16 +57,20 @@ public class task10 {
         System.out.println("*************** Point A ******************************");
         System.out.println("The item on Main Page: " + listMainPage[0]);
         System.out.println("The item on Item Page: " + listItemPage[0]);
+        assertEquals(listMainPage[0], listItemPage[0]);
         printResult(listMainPage[0], listItemPage[0], "name");
+
 
         System.out.println("*************** Point B ******************************");
         System.out.println("The regular price on Main Page: " + listMainPage[1]);
         System.out.println("The regular price on Item Page: " + listItemPage[1]);
         printResult(listMainPage[1], listItemPage[1], "regular price");
+        assertEquals(listMainPage[1], listItemPage[1]);
 
         System.out.println("The campaign price on Main Page: " + listMainPage[5]);
         System.out.println("The campaign price on Item Page: " + listItemPage[5]);
         printResult(listMainPage[5], listItemPage[5], "campaign price");
+        assertEquals(listMainPage[5], listItemPage[5]);
 
         System.out.println("*************** Point C ******************************");
         System.out.println("Main Page");
@@ -65,17 +83,32 @@ public class task10 {
         System.out.println("Regular price (text decoration): " + listItemPage[3]);
         System.out.println("Campaign price (color): " + listItemPage[6]);
         System.out.println("Campaign price (weight): " + listItemPage[7] + "\n");
+        assertEquals("цвет не серый!",listMainPage[2], "rgb(119, 119, 119)");
+        assertEquals("цвет не серый или текст не зачёркнутый!", listMainPage[3], "line-through rgb(119, 119, 119)");
+        assertEquals("цвет не красный!", listMainPage[6], "rgb(204, 0, 0)");
+        MatcherAssert.assertThat("цвет не красный!", listMainPage[6], endsWith("0, 0)"));
+        assertEquals(listMainPage[7], "900");
+        assertEquals("цвет не серый!", listItemPage[2], "rgb(102, 102, 102)");
+        assertEquals("цвет не серый или текст не зачёркнутый!", listItemPage[3], "line-through rgb(102, 102, 102)");
+        assertEquals("цвет не красный!", listItemPage[6], "rgb(204, 0, 0)");
+        MatcherAssert.assertThat("цвет не красный!", listMainPage[6], endsWith("0, 0)"));
+        assertEquals(listItemPage[7], "700");
 
         System.out.println("*************** Point D ******************************");
         System.out.println("Main Page");
         System.out.println("Regular price (font size):" + listMainPage[4]);
         System.out.println("Campaign price (font size): " + listMainPage[8]);
         printComp(listMainPage[8], listMainPage[4]);
+        assertEquals(listMainPage[4], "14.4px");
+        assertEquals(listMainPage[8], "18px");
 
         System.out.println("Item Page");
         System.out.println("Regular price (font size): " + listItemPage[4]);
         System.out.println("Campaign price (font size): " + listItemPage[8]);
         printComp(listItemPage[8], listItemPage[4]);
+        assertEquals(listItemPage[4], "16px");
+        assertEquals(listItemPage[8], "22px");
+
     }
 
     private String[] fillList (WebElement root) {
@@ -93,11 +126,12 @@ public class task10 {
         return list;
     }
 
-    private void printList(String[] list){
+
+    /*private void printList(String[] list){
         for (String s:list)
             System.out.println(s);
         System.out.println();
-    }
+    }*/
 
     private void printResult(String s1, String s2, String key) {
         String result = "The " + key + " of the items is ";
@@ -108,16 +142,20 @@ public class task10 {
         System.out.println(result);
     }
 
+
+
     private void printComp(String s1, String s2) {
         String result;
-        if (s1.compareTo(s2) > 0)
+        s1= s1.replaceAll("[^0-9].", "");
+        s2= s2.replaceAll("[^0-9].", "");
+        double x1 = Integer.parseInt(s1);
+        double x2 = Integer.parseInt(s2);
+        if (x1 > x2)
             result = "bigger";
         else
             result = "smaller";
         System.out.println("CampaignPrice Font " + result + " then RegularPrice Font.");
     }
-
-
 
     @After
     public void stop() {
